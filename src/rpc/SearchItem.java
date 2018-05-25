30a08c606d425392a29c8c6c7ff5f3a6b9d3f25c
luolulu <luo@luodembp.attlocal.net>
2018-05-25 14:47:42 -0500

    connect database with mysql

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
-import org.json.JSONException;
+
 import org.json.JSONObject;
 
 import entity.Item;
